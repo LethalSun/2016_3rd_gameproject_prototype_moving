1e9a1b71399b1851c40bc66fe168a4e98c4c3e99
DESKTOP-TB68M19\LongShore <massivedynamicnorth@gmail.com>
2016-10-22 20:46:21 +0900

    빨간줄 수정

--- PrototypeForMovement/Classes/MakeAnimation.cpp
+++ PrototypeForMovement/Classes/MakeAnimation.cpp
@@ -6,18 +6,19 @@ const float HIT_ANIMATION_SPEED = 0.1f;
 
 void MakeAnimation::AnimationMove()
 {
+	int imageStartNumber = 0;
 	m_Character->stopAllActions();
 	Vector<SpriteFrame*> animFrame;
 	for (int i = imageStartNumber; i < imageStartNumber + 8; i++)
 	{
-		sprintf(m_buffer, "%s%d%s", m_FrameName,i,m_FileNameExtention);
+		sprintf(m_buffer, "%s%d%s", m_FrameName, i, m_FileNameExtention);
 
 		auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(m_buffer);
 		animFrame.pushBack(frame);
 	}
 	m_pAnimation = Animation::createWithSpriteFrames(animFrame, ANIMATION_SPEED);
 	m_pAnimate = Animate::create(m_pAnimation);
-	m_pArchBishop->runAction(RepeatForever::create(m_pAnimate));
+	m_Character->runAction(RepeatForever::create(m_pAnimate));
 }
 
 void MakeAnimation::AnimationStop()
@@ -30,8 +31,7 @@ void MakeAnimation::AnimationAttack()
 
 void MakeAnimation::MakeAnimationWithFrameName(const char & plistName, const char & frameName, Sprite * character)
 {
-	sprintf(m_FileNameExtention,"%s", plistName);
+	sprintf(m_FileNameExtention, "%s", plistName);
 	sprintf(m_FrameName, "%s", frameName);
 	m_Character = character;
 }
-
