c30485f3d888616f15c9d9c937e20ff083a640e1
DESKTOP-TB68M19\LongShore <massivedynamicnorth@gmail.com>
2016-10-25 00:37:08 +0900

    애니메이션의 온오프를 나타내는 변수 추가 그리고 애니메이션을 미리 만들어놓고 필요할때마다 불러서 쓰는 방식으로 변경중

--- PrototypeForMovement/Classes/MakeAnimation.cpp
+++ PrototypeForMovement/Classes/MakeAnimation.cpp
@@ -12,7 +12,7 @@
 //	return true;
 //}
 //
-//이동 애니메이션을 만들어 반환한다.
+//이동 애니메이션을 만든다.
 Animate* MakeAnimation::AnimationMove(int direction)
 {
 	int imageStartNumber = direction * BE_IDCA_DEFINES::NUM_OF_SPRITE_FILE_PER_ACTIONS;
@@ -20,7 +20,9 @@ Animate* MakeAnimation::AnimationMove(int direction)
 	for (int i = imageStartNumber; i < imageStartNumber + 8; i++)
 	{
 		sprintf(m_Buffer, "%smove%d%s", m_FrameName, i, m_FileNameExtention);
-
+		char buffer[256];
+		sprintf(buffer, "inputKeyboard: %s", m_Buffer);
+		cocos2d::log(buffer);
 		auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(m_Buffer);
 		animFrame.pushBack(frame);
 	}
@@ -31,7 +33,7 @@ Animate* MakeAnimation::AnimationMove(int direction)
 	return m_pAnimate;
 }
 
-//정지상태의 텍스쳐를 반환한다.
+//정지상태의 텍스쳐를 만든다.
 Animate* MakeAnimation::AnimationStop(int direction)
 {
 	int imageStartNumber = direction;
@@ -45,7 +47,7 @@ Animate* MakeAnimation::AnimationStop(int direction)
 	return m_pAnimate;
 }
 
-//공격 애니메이션을 반환한다.
+//공격 애니메이션을 만든다.
 Animate* MakeAnimation::AnimationAttack(int direction)
 {
 	int imageStartNumber = (direction | BE_IDCA_ACTIONS::ACTIONS::ATTACK) * BE_IDCA_DEFINES::NUM_OF_SPRITE_FILE_PER_ACTIONS;
@@ -58,7 +60,10 @@ Animate* MakeAnimation::AnimationAttack(int direction)
 	Vector<SpriteFrame*> animFrame;
 	for (int i = imageStartNumber; i < imageStartNumber + 7; i++)
 	{
-		sprintf(m_Buffer, "%shit%d%s", m_FrameName, direction, m_FileNameExtention);
+		sprintf(m_Buffer, "%shit%d%s", m_FrameName, i, m_FileNameExtention);
+		char buffer[256];
+		sprintf(buffer, "inputKeyboard: %s", m_Buffer);
+		cocos2d::log(buffer);
 		auto frame = SpriteFrameCache::getInstance()->getSpriteFrameByName(m_Buffer);
 		animFrame.pushBack(frame);
 	}
@@ -71,10 +76,66 @@ Animate* MakeAnimation::AnimationAttack(int direction)
 	return m_pAnimate;
 }
 
+Animate * MakeAnimation::GetAttackAnimation(int direction)
+{
+	*m_pAttackUpUpLeft;
+	*m_pAttackLeftDownLeft;
+	*m_pAttackDownDownRight;
+	*m_pAttackRightUpRight;
+
+	return nullptr;
+}
+
+Animate * MakeAnimation::GetMoveAnimation(int direction)
+{
+	*m_pMoveUp;
+	*m_pMoveDown;
+	*m_pMoveRight;
+	*m_pMoveLeft;
+	*m_pMoveUpLeft;
+	*m_pMoveUpRight;
+	*m_pMoveDownRight;
+	*m_pMoveDownLeft;
+	return nullptr;
+}
+
+Animate * MakeAnimation::GetStopAnimation(int direction)
+{
+	*m_pStopUp;
+	*m_pStopDown;
+	*m_pStopRight;
+	*m_pStopLeft;
+	*m_pStopUpLeft;
+	*m_pStopUpRight;
+	*m_pStopDownRight;
+	*m_pStopDownLeft;
+	return nullptr;
+}
+
 MakeAnimation::MakeAnimation(const char const *fileName, const char const * fileNameExtention)
 {
 	sprintf(m_FileNameExtention, "%s", fileNameExtention);
 	sprintf(m_FrameName, "%s", fileName);
+	m_pMoveUp = AnimationMove(1);
+	m_pMoveDown = AnimationMove(2);
+	m_pMoveRight = AnimationMove(4);
+	m_pMoveLeft = AnimationMove(8);
+	m_pMoveUpLeft = AnimationMove(9);
+	m_pMoveUpRight = AnimationMove(5);
+	m_pMoveDownRight = AnimationMove(6);
+	m_pMoveDownLeft = AnimationMove(10);
+	m_pAttackUpUpLeft = AnimationAttack(1);
+	m_pAttackLeftDownLeft = AnimationAttack(8);
+	m_pAttackDownDownRight = AnimationAttack(2);
+	m_pAttackRightUpRight = AnimationAttack(4);
+	m_pStopUp = AnimationStop(1);
+	m_pStopDown = AnimationStop(2);
+	m_pStopRight = AnimationStop(4);
+	m_pStopLeft = AnimationStop(8);
+	m_pStopUpLeft = AnimationStop(9);
+	m_pStopUpRight = AnimationStop(5);
+	m_pStopDownRight = AnimationStop(6);
+	m_pStopDownLeft = AnimationStop(10);
 }
 
 MakeAnimation::~MakeAnimation()
